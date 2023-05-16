7301c1c83ef97f740440fdfe442c63268723e94b
Pierrick Catalo <43609852+PierrickPro@users.noreply.github.com>
2023-05-16 15:31:07 -0400

    first commit

---
+++ shopApp/shopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopAppApp.swift
+//  shopApp
+//
+//  Created by Pierrick Catalo on 2023-05-16.
+//
+
+import SwiftUI
+
+@main
+struct shopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
